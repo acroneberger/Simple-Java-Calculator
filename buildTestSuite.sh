8a1b504dc16e92655405c32ddbd5de5db91317cb
Andrew Croneberger <acroneberger@gmail.com>
2020-06-03 14:32:12 -0700

    Add timing and email functionality to run script

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd src/simplejavacalculator/
+pwd
 rm -rf testClasses
 mkdir testClasses
 javac128="/etc/alternatives/java_sdk_11_openjdk/bin/javac -J-Xms128m -J-Xmx128m"
--- runTestSuite.sh
+++ runTestSuite.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+
 cd src/simplejavacalculator
 numLoops=$1
 optionalEmail=$2
-
-echo $1
-echo $2
-
+echo "Starting test at $(date)" > testout.txt
 
 for i in $(seq 1 $numLoops)
 do
 echo $i
-java -Xms128m -Xmx128m -jar junit-platform-console-standalone-1.6.2.jar --class-path testClasses --scan-class-path
+java -Xms128m -Xmx128m\
+ -jar junit-platform-console-standalone-1.6.2.jar\
+ --class-path testClasses --scan-class-path | tee -a testout.txt 
+result=$?
+echo $result
 done
+
+echo "Ending test at $(date)" >> testout.txt
+
+if [ $# -eq 2 ]
+then
+echo "sending email to $optionalEmail"
+echo "your test results are included" | mailx -s' test output' -a testout.txt $optionalEmail
+fi
+
+
+echo "done"
