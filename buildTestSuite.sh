8442643514b0a6e5d5a16c9bf3a6f52f4296b557
Andrew Croneberger <acroneberger@gmail.com>
2020-06-03 16:45:54 -0700

    Add error reporting for compilation

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -7,7 +7,11 @@ javac128="/etc/alternatives/java_sdk_11_openjdk/bin/javac -J-Xms128m -J-Xmx128m"
 
 $javac128 -d testClasses Calculator.java BufferedImageCustom.java
 echo "compiling calculator"
-#$javac128 -d testClasses BufferedImageCustom.java
 $javac128 -d testClasses -cp testClasses:junit-platform-console-standalone-1.6.2.jar CalculatorTest.java  BufferedImageCustomTest.java
 echo "compiling unit tests"
-#$javac128 -d testClasses -cp testClasses:junit-platform-console-standalone-1.6.2.jar TestSuite.java
+if [ $# -ne 0 ]
+then
+echo "An error occurred during compilation. Exiting..."
+exit 1
+fi
+echo "Compilation complete!"
