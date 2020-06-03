0bb57a02691316fed46c58e23a694b673906c7ee
Andrew Croneberger <acroneberger@gmail.com>
2020-06-03 16:43:03 -0700

    add result parsing and multiple test unit compilation to build and run scripts

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -5,7 +5,9 @@ rm -rf testClasses
 mkdir testClasses
 javac128="/etc/alternatives/java_sdk_11_openjdk/bin/javac -J-Xms128m -J-Xmx128m"
 
-$javac128 -d testClasses Calculator.java
+$javac128 -d testClasses Calculator.java BufferedImageCustom.java
 echo "compiling calculator"
-$javac128 -d testClasses -cp testClasses:junit-platform-console-standalone-1.6.2.jar CalculatorTest.java
+#$javac128 -d testClasses BufferedImageCustom.java
+$javac128 -d testClasses -cp testClasses:junit-platform-console-standalone-1.6.2.jar CalculatorTest.java  BufferedImageCustomTest.java
 echo "compiling unit tests"
+#$javac128 -d testClasses -cp testClasses:junit-platform-console-standalone-1.6.2.jar TestSuite.java
--- runTestSuite.sh
+++ runTestSuite.sh
@@ -12,15 +12,35 @@ java -Xms128m -Xmx128m\
  -jar junit-platform-console-standalone-1.6.2.jar\
  --class-path testClasses --scan-class-path | tee -a testout.txt 
 result=$?
-echo $result
+if [ $? -ne 0 ]
+then
+echo "An error occurred on running the tests. Exiting..."
+exit 1
+fi
 done
 
+#process results (use only last test in the event that multiple tests were run)
+totaltests=$(grep -o -P "(\d+) tests found" testout.txt | tail -1 | cut -d ' ' -f 1)
+successtests=$(grep -o -P "(\d+) tests successful" testout.txt | tail -1 | cut -d ' ' -f 1)
+failtests=$(grep -o -P "(\d+) tests failed" testout.txt | tail -1 | cut -d ' ' -f 1)
+
 echo "Ending test at $(date)" >> testout.txt
+
+
 
 if [ $# -eq 2 ]
 then
 echo "sending email to $optionalEmail"
-echo "your test results are included" | mailx -s' test output' -a testout.txt $optionalEmail
+if [ $failtests -eq 0 ]
+then
+status='PASSED'
+msg="All tests passed, results are included in attached document"
+else
+status='FAILED'
+msg="$failtests tests failed, results are included in attached document."
+echo $msg
+fi
+echo "$msg" | mailx -s "Results from test run: $status" -a testout.txt $optionalEmail
 fi
 
 
